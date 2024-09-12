8f1955a219a9d058ea6a766e78ec192dd9965921
marwa <Ifonll>
2024-09-12 13:22:24 -0400

    :)

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String)
-    hashed_password = Column(String)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250))
+    hashed_password = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
